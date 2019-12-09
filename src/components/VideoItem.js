e47f5da971f7afdf31645df01e7718780d4dda66
michal <mpruchniak@gmail.com>
2019-12-09 22:54:40 +0100

    stylish video items

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const VideoItem = ({ video })=> {
-return <div>{video.snippet.title}</div>;
+return(
+    <div className="item">
+        <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+        <div className="content">
+            <div class="header">{video.snippet.title}</div>
+        </div>
+
+            
+    </div>
+);
 }
 
 export default VideoItem;
