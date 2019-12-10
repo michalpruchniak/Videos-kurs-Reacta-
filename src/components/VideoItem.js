a6aa5f47b1a4a6e28391021085e8f29e42310051
michal <mpruchniak@gmail.com>
2019-12-10 21:53:59 +0100

    grid system

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoItem = ({ video, onVideoSelect })=> {
 return(
     <div onClick={() => onVideoSelect(video) } className="item">
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title    } />
+        <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title } />
         <div className="content">
             <div className="header">{video.snippet.title}</div>
         </div>
